255590a1b68a8c19a19c9c478c3c942e2508475a
宋培雨 <2111467955@qq.com.com>
2022-11-26 18:47:54 +0800

    ''

---
+++ js/setRem.js
@@ -0,0 +1,11 @@
+
+function setRem() {
+    var clientWidth = document.clientWidth || document.body.clientWidth;
+    clientWidth = clientWidth > 1920 ? 1920 : clientWidth;
+    clientWidth = clientWidth < 1024 ? 1024 : clientWidth;
+    var rem = clientWidth / 18;
+    var html = document.getElementsByTagName('html')[0];
+    html.style.fontSize = rem + 'px';
+}
+window.onload = setRem;
+window.onresize = setRem;
